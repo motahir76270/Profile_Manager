c069784b6045773a7175ce3b7699dc963f5712e5
tahir76270 <motahir76270>
2025-09-05 23:09:19 +0530

    frontend change

--- frontend/src/componnets/Home.jsx
+++ frontend/src/componnets/Home.jsx
@@ -23,11 +23,12 @@ function Home() {
      {/* All profile list container */}
     <main className='bg-gradient-to-br from-sky-900 via-slate-600 to-sky-800 py-5  grid-cols-1 md:grid-cols-2 lg:grid-cols-3  px-10 gap-5'>
       <div className='flex justify-between'>
-        <div className='flex gap-[2rem]'>
-      <h1 className='text-3xl text-gray-300  '>All Profiles</h1>
-      <Link className='rounded-sm bg-green-500 py-2 px-3 ' to="/addProfile">Add New Profile</Link>
-        </div>
-      <input type="text" placeholder='Search' onChange={(e) => setProfileSearch(e.target.value)} className='px-5 w-60 rounded-sm bg-white/30 shadow-2xl shadow-gray-900 ' />
+       
+   <div className='flex flex-col md:flex-row md:gap-[2rem]'>
+      <h1 className='md:text-3xl text-gray-300  '>All Profiles</h1>
+      <Link className='rounded-sm bg-green-500 w-35 h-8 py-1 px-2 md:w-40 md:py-2 md:px-3 ' to="/addProfile">Add New Profile</Link>
+   </div>
+      <input type="text" placeholder='Search' onChange={(e) => setProfileSearch(e.target.value)} className='px-5 w-40 h-8 md:w-60 rounded-sm bg-white/30 shadow-2xl shadow-gray-900 ' />
     </div>
       <ProfileCard profileSearch={profileSearch} />
     </main>
